28a944a4be278c7507058c1144b9ff5acd6e9189
ismael <ismaelcompsci@gmail.com>
2023-02-20 14:43:16 -0600

    user register

--- expenseswebsite/authentication/views.py
+++ expenseswebsite/authentication/views.py
@@ -1,7 +1,33 @@
+from django.contrib.auth.models import User
+import json
+from django.http import JsonResponse
+
 from django.shortcuts import render
 from django.views import View
 
 # Create your views here.
+
+
+class UsernameValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        username = data["username"]
+
+        if not str(username).isalnum():
+            return JsonResponse(
+                {
+                    "username_error": "username should only contain alphanumeric characters"
+                },
+                status=400,
+            )
+        if User.objects.filter(username=username).exists():
+            return JsonResponse(
+                {
+                    "username_error": "username should only contain alphanumeric characters"
+                },
+                status=400,
+            )
+        return JsonResponse({"username_valid": True})
 
 
 class RegistrationView(View):
