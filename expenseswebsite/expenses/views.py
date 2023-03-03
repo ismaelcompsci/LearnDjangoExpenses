4e282e3dc365434beb35fd6bfe5e9178bc7e57b0
ismael <ismaelcompsci@gmail.com>
2023-03-03 15:19:35 -0600

    CRUD

--- expenseswebsite/expenses/views.py
+++ expenseswebsite/expenses/views.py
@@ -1,14 +1,94 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import cache_control
+from django.contrib import messages
+
+from .models import Category, Expense
 
 
 # Create your views here.
 @login_required(login_url="authentication/login")
 @cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def index(request):
-    return render(request, "expenses/index.html")
+    expeneses = Expense.objects.filter(owner=request.user)
+
+    context = {"expenses": expeneses}
+
+    return render(request, "expenses/index.html", context)
 
 
 def add_expense(request):
-    return render(request, "expenses/add_expense.html")
+    categories = Category.objects.all()
+    context = {"categories": categories, "values": request.POST}
+
+    if request.method == "GET":
+        return render(request, "expenses/add_expense.html", context=context)
+
+    if request.method == "POST":
+        amount = request.POST["amount"]
+        description = request.POST["description"]
+        category = request.POST["category"]
+        date = request.POST["expense_date"]
+
+        if not amount:
+            messages.error(request, "Amount is required")
+            return render(request, "expenses/add_expense.html", context=context)
+
+        if not description:
+            messages.error(request, "Description is required")
+            return render(request, "expenses/add_expense.html", context=context)
+
+        Expense.objects.create(
+            amount=amount,
+            date=date,
+            category=category,
+            description=description,
+            owner=request.user,
+        )
+        messages.success(request, "Expense saved succesfully")
+
+        return redirect("expenses")
+
+
+def expense_edit(request, id):
+    expense = Expense.objects.get(pk=id)
+    categories = Category.objects.all()
+
+    context = {"expense": expense, "values": expense, "categories": categories}
+
+    if request.method == "GET":
+        return render(request, "expenses/edit-expense.html", context)
+
+    if request.method == "POST":
+        amount = request.POST["amount"]
+        description = request.POST["description"]
+        category = request.POST["category"]
+        date = request.POST["expense_date"]
+
+        if not amount:
+            messages.error(request, "Amount is required")
+            return render(request, "expenses/edit-expense.html", context=context)
+
+        if not description:
+            messages.error(request, "Description is required")
+            return render(request, "expenses/edit-expense.html", context=context)
+
+        expense.owner = request.user
+        expense.date = date
+        expense.amount = amount
+        expense.category = category
+        expense.description = description
+
+        expense.save()
+
+        messages.success(request, "Expense Updated succesfully")
+
+        return redirect("expenses")
+
+
+def delete_expense(request, id):
+    expense = Expense.objects.get(pk=id)
+    expense.delete()
+    messages.success(request, "Expense removed")
+
+    return redirect("expenses")
