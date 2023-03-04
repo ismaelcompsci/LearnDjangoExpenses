77a34f2ed3f008cb1d8d9ee2a8888bf6cd44940c
ismael <ismaelcompsci@gmail.com>
2023-03-04 17:04:53 -0600

    search expenses

--- expenseswebsite/expenses/views.py
+++ expenseswebsite/expenses/views.py
@@ -1,9 +1,38 @@
+import json
+from django.http import JsonResponse
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import cache_control
 from django.contrib import messages
 
+
 from .models import Category, Expense
+
+
+def search_expenses(request):
+    if request.method == "POST":
+        search_str = json.loads(request.body).get("searchText")
+
+        expenses = (
+            Expense.objects.filter(
+                amount__istartswith=search_str,
+                owner=request.user,
+            )
+            | Expense.objects.filter(
+                date__istartswith=search_str,
+                owner=request.user,
+            )
+            | Expense.objects.filter(
+                description__icontains=search_str,
+                owner=request.user,
+            )
+            | Expense.objects.filter(
+                category__icontains=search_str,
+                owner=request.user,
+            )
+        )
+        data = expenses.values()
+        return JsonResponse(list(data), safe=False)
 
 
 # Create your views here.
---
+++ expenseswebsite/expenseswebsite/static/js/searchExpenses.js
@@ -0,0 +1,48 @@
+const searchField = document.querySelector("#searchField");
+const tableOutput = document.querySelector(".table-output");
+const appTable = document.querySelector(".app-table");
+const paginationContainer = document.querySelector(".pagination-container");
+const tableBody = document.querySelector(".table-body");
+
+tableOutput.style.display = "none";
+
+searchField.addEventListener("keyup", (e) => {
+  const searchValue = e.target.value;
+
+  if (searchValue.trim().length > 0) {
+    paginationContainer.style.display = "none";
+    tableBody.innerHTML = "";
+    console.log(searchValue);
+
+    fetch("/search-expenses", {
+      body: JSON.stringify({ searchText: searchValue }),
+      method: "POST",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        appTable.style.display = "none";
+        tableOutput.style.display = "block";
+
+        if (data.length === 0) {
+          tableOutput.innerHTML = "No results found";
+        } else {
+          data.forEach((item) => {
+            tableBody.innerHTML += `
+        <tr>
+            <td>${item.amount}</td>
+            <td>${item.category}</td>
+            <td>${item.description}</td>
+            <td>${item.date}</td>
+
+        </tr>
+            `;
+          });
+        }
+      });
+  } else {
+    appTable.style.display = "block";
+    paginationContainer.style.display = "block";
+    tableOutput.style.display = "none";
+  }
+});
